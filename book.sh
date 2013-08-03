84e5e7e513b743a0a482d0bb0ce74bafd5a8f39b
nyo <billy31210@gmail.com>
2013-08-04 01:40:16 +0800

    adjust breaking bad

--- book.sh
+++ book.sh
@@ -5,13 +5,23 @@ _uninstall() {
     rm ~/.conky ~/.conkyrc
 }
 
+_start() {
+    ~/conky_book/conky.start.sh
+}
+
+_stop() {
+    killall conky
+}
+
 install_breaking_bad() {
     _uninstall
 
     #create new link of conky
-    ln -s ~/conky_book/package/breaking\ bad\ conky/.conky ~/.conky
-    ln -s ~/conky_book/package/breaking\ bad\ conky/conkyrc ~/.conkyrc
-    cp ~/conky_book/package/breaking\ bad\ conky/fonts/* ~/.fonts
+    ln -s ~/conky_book/package/breaking_bad/conky ~/.conky
+    ln -s ~/conky_book/package/breaking_bad/conkyrc ~/.conkyrc
+
+    #install fonts
+    cp ~/conky_book/package/breaking_bad/fonts/* ~/.fonts
     fc-cache -fv
 }
 
@@ -28,16 +38,10 @@ install_conky_joey() {
 
     #create new link of conky
     ln -s ~/conky_book/package/Conky-Joey/conkyrc ~/.conkyrc
+
+    #install fonts
     cp ~/conky_book/package/Conky-Joey/fonts/* ~/.fonts
     fc-cache -fv
-}
-
-_start() {
-    ~/conky_book/conky.start.sh
-}
-
-_stop() {
-    killall conky
 }
 
 case $1 in
@@ -57,8 +61,8 @@ case $1 in
         _start
         ;;
     restart)
-        _start
         _stop
+        _start
         ;;
     stop)
         _stop
